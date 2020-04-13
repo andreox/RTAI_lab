a5b93c1902447838bc6c803b44ae71e64a03d1ef
andreox <andreozzi.alessio@gmail.com>
2020-04-13 19:00:06 +0200

    Update homework.c

--- homework.c
+++ homework.c
@@ -4,7 +4,7 @@
 #include <rtai_sched.h>
 #include <rtai_shm.h>
 #include <stdbool.h>
-#include <stdlib.h>
+
 
 
 #define SHMNAM 1234
@@ -32,54 +32,63 @@ static struct rec_struct *rec ;
 
 
 
-static void set_bit( int index ) {
+static void set__bit( int index ) {
 
-	if ( bits[index] == 1 ) bits[index] = 0 ;
-	else bits[index] = 1 ;
-	rt_printk("BIT %d VALUE : %d\n",index,bits[index]) ;
-
+	while(1) {
+		if ( bits[index] == 1 ) bits[index] = 0 ;
+		else bits[index] = 1 ;
+		rt_printk("BIT %d VALUE : %d\n",index,bits[index]) ;
+		rt_task_wait_period() ;
+	}
 	return;
 
 }
 
-static void reset_states() {
+static void reset_states(void) {
 
 	int k ;
 	for ( k = 0 ; k < 4 ; k++ ) state[k] = false ;
 
 }
 
-static void recognize() {
+static void recognize(void) {
 
-	int num = 2*2*bits[2] + 2*bits[1] + bits[0] ;
+	int num,j ;
+	while(1) {
+		num = 4*bits[2] + 2*bits[1] + bits[0] ;
 
-	int j ;
-	for ( j = 0 ; j < 4 && state[j] ; j++ ) ;
+		rt_printk("Numero letto : %d\n N.Seq ric : %d\n",num,rec->count);
+		
+		for ( j = 0 ; j < 4 && state[j] ; j++ ) {}
 
-	if ( j == 4 ) {
-		rec->OK = 1 ;
-		rec->count++ ;
-		rt_printk(" REC->OK : 1, REC->COUNT : %d\n\n",rec->count) ;
-
-		reset_states() ;
-	}
-
-	else if ( !state[j] && num == val_states[j] ) {
-
-		state[j] = true ;
-		if ( j == 3 ) {
-
+		if ( j == 4 ) {
 			rec->OK = 1 ;
-			rec->count++ ;
-			rt_printk("REC->OK : 1, REC->COUNT : %d\n\n",rec->count) ;
+			rec->count = rec->count + 1 ;
+			rt_printk(" REC->OK : 1, REC->COUNT : %d\n\n",rec->count) ;
+
 			reset_states() ;
 		}
 
-	else { //ho riconosciuto 0,3 e poi un numero diverso, devo risettare gli stati
+		else if ( !state[j] && num == val_states[j] ) {
 
-		reset_states() ;
-	}
+			state[j] = true ;
+			if ( j == 3 ) {
+
+				rec->OK = 1 ;
+				rec->count = rec->count + 1 ;
+				rt_printk("REC->OK : 1, REC->COUNT : %d\n\n",rec->count) ;
+				reset_states() ;
+			}
+
+		}
+
+		else { //ho riconosciuto 0,3 e poi un numero diverso, devo risettare gli stati
+
+			reset_states() ;
+		}
 	
+		rt_task_wait_period() ;
+	}
 	return;
 	
 }
@@ -93,24 +102,24 @@ int init_module(void) {
 	for ( i = 0 ; i < NTASKS-1 ; i++ ) {
 
 		bits[i] = 0 ;
-		rt_task_init( &thread[i] , fun, i, STACK_SIZE, NTASKS - i , 0, 0) ;
+		rt_task_init( &thread[i] , (void*)set__bit, i, STACK_SIZE, NTASKS - i , 0, 0) ;
 	}
 
 
-	rt_task_init( &thread[3], recognize, NULL, STACK_SIZE, 5, 0, 0 ) ;
-	for ( i = 0 ; i < 3 ; i++ ) state[i] = false ;
+	rt_task_init( &thread[3], (void*)recognize, 0, STACK_SIZE, 5, 0, 0 ) ;
+	reset_states() ;
 	rec = rtai_kmalloc( SHMNAM, sizeof(struct rec_struct) );
 	rec->OK = 0 ;
 	rec->count = 0 ;
 
 	periodo = nano2count( TICK_PERIOD ) ;
 
-	for ( i = 0 ; i < NTASKS-1 ; i++ ) {
+	
+	rt_task_make_periodic( &thread[0], rt_get_time()+periodo , periodo) ;
+	rt_task_make_periodic( &thread[1], rt_get_time()+(periodo*2), periodo*2 ) ;
+	rt_task_make_periodic( &thread[2], rt_get_time()+(periodo*4), periodo*4) ;
 
-		rt_make_periodic( &thread[i], rt_get_time()+(periodo*(i+1)) , periodo*(i+1)) ;
-	}
-
-	rt_make_periodic( &thread[3], rt_get_time()+(periodo*5), periodo) ;
+	rt_task_make_periodic( &thread[3], rt_get_time()+(periodo*5), periodo) ;
 
 	rt_spv_RMS(0) ;
 
